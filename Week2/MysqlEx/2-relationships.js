6690e4d40ec41334b324d74bfad07f3af838a4fa
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-08-29 13:20:55 +0200

    databases-week2-hasan

--- Week2/MysqlEx/2-relationships.js
+++ Week2/MysqlEx/2-relationships.js
@@ -49,23 +49,6 @@ const executeQueries = (queries) => {
 
 const queries = [
   `
-    CREATE TABLE authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(255) NOT NULL,
-    university VARCHAR(255),
-    date_of_birth DATE,
-    h_index INT,
-    gender ENUM('Male', 'Female', 'Other')
-    );
-    `,
-  `ALTER TABLE authors
-    ADD COLUMN mentor INT;
-    `,
-  `ALTER TABLE authors
-    ADD FOREIGN KEY (mentor) 
-    REFERENCES authors(author_id);
-    `,
-  `
     CREATE TABLE research_Papers (
         paper_id INT AUTO_INCREMENT PRIMARY KEY,
         paper_title VARCHAR(255) NOT NULL,
